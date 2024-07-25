dd23c49b2288bc5e5546b57bcbbc78f0581c423c
Raksha Shanbhag <r2shanbh@gmail.com>
2024-07-25 06:32:21 +0400

    RSVP Tracker files

---
+++ src/main/java/com/raksha/rsvp_tracker_service/RsvpTrackerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.raksha.rsvp_tracker_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsvpTrackerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsvpTrackerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raksha/rsvp_tracker_service/RsvpTrackerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raksha.rsvp_tracker_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsvpTrackerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
